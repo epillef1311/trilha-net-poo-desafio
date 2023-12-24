17cbe1464925da129eb1e96eeed2d95070ee3c8d
Fellipe <fellipe1311.fb@gmail.com>
2023-12-24 17:26:51 -0300

    Finalizado desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia(numero: "9949192", modelo:"Modelo X25S1", imei:"1235213", memoria:128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Subway surfers");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone(numero: "8548143", modelo:"Modelo I2023", imei:"7895213", memoria:64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Tetris");
 // TODO: Realizar os testes com as classes Nokia e Iphone
